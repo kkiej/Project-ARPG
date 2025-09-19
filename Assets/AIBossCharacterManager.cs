42ce6b482898235d93090e46b2bc1aa17488bdb8
kkiej <1937664992@qq.com>
2025-09-19 10:44:53 +0800

    EP.42

---
+++ Assets/AIBossCharacterManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LZ
+{
+    public class AIBossCharacterManager : MonoBehaviour
+    {
+        //  GIVE THIS A.I A UNIQUE ID
+        //  WHEN THIS A.I IS SPAWNED, CHECK OUR SAVE FILE (DICTIONARY)
+        //  IF THE SAVE FILE DOES NOT CONTAIN A BOSS MONSTER WITH THIS I.D ADD IT
+        //  IF IT IS PRESENT, CHECK IF THE BOSS HAS BEEN DEFEATED
+        //  IF THE BOSS HAS BEEN DEFEATED, DISABLE THIS GAMEOBJECT
+        //  IF THE BOSS HAS NOT BEEN DEFEATED, ALLOW THIS OBJECT TO CONTINUE TO BE ACTIVE
+    }
+}
